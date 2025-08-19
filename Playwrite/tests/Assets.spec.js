899ebdd51c1a81e5ce2139180b0ddea3d8f86cc5
Golam Mostafa Imran <39483919+imrantista@users.noreply.github.com>
2025-08-19 13:16:06 +0600

    Update Assets.spec.js

--- Playwrite/tests/Assets.spec.js
+++ Playwrite/tests/Assets.spec.js
@@ -1,38 +1,38 @@
 import { test, expect } from "@playwright/test";
-import CommonActions from "../Pages/Common/Actions.js";
+import LoginSession from "../Pages/Common/LoginSession.js";
+import SendResult from "../Pages/Common/SendResult.js";
 import AddNewAsset from "../Pages/Assets/AddNewAsset.js";
 import AssetDelete from "../Pages/Assets/AssetDelete.js";
 import testData from "../fixtures/testData.json" assert { type: "json" };
 test.describe("Assets Module Tests", () => {
   let resultTracker;
-  let commonAction;
-  test.beforeEach(async () => {
+  const loginSession = new LoginSession();
+  const addNewAsset = new AddNewAsset();
+  const deleteAsset = new AssetDelete();
+  const mailer = new SendResult();
+
+  test.beforeEach(async ({ page }) => {
     resultTracker = {
       successCount: 0,
       errorCount: 0,
       errorMessages: [],
       stepToReproduces: [],
     };
-    commonAction = new CommonActions();
-  });
-  test.beforeEach(async ({ page }) => {
-    await commonAction.loginSession(page);
+    await loginSession.login(page);
+    await page.goto(`${testData.link}products-and-assets?tab=Assets`);
+    await expect(page.locator("text=Search")).toBeVisible();
   });
 
   test("Add new Asset", async ({ page }) => {
-    await page.goto(`${testData.link}products-and-assets?tab=Assets`);
-    await expect(page.locator("text=Search")).toBeVisible();
     await page.getByRole("button", { name: "Add new" }).click();
-    const addNewAsset = new AddNewAsset();
     await addNewAsset.addAsset(page, resultTracker);
   });
+
   test.only("Delete Asset", async ({ page }) => {
-    await page.goto(`${testData.link}products-and-assets?tab=Assets`);
-    await expect(page.locator("text=Search")).toBeVisible();
-    const deleteAsset = new AssetDelete();
     await deleteAsset.deleteAsset(page, resultTracker);
   });
+
   test.afterAll(async () => {
-    await commonAction.logResults(resultTracker, "Assets Module");
+    await mailer.logResults(resultTracker, "Assets Module");
   });
 });
